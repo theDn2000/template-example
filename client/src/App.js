53637ba7009c35fd3f96523e510e8ecd6db5bc47
dcarneros <theDn2000@gmail.com>
2022-12-28 16:41:27 +0100

    V7.1

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,4 @@
-import React, { Component } from 'react';
-import {usestate} from 'react';
-import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
-import Home from './Home';
-import About from './About';
-import Contact from './Contact';
-import './App.css';
+import {useState} from 'react';
 
 const App = () => {
   const [data, setData] = useState({data: []});
@@ -15,8 +9,8 @@ const App = () => {
     setIsLoading(true);
 
     try {
-      const response = await fetch('api/comprobarusuario', {
-        method: 'POST',
+      const response = await fetch('https://reqres.in/api/users', {
+        method: 'GET',
         headers: {
           Accept: 'application/json',
         },
@@ -40,27 +34,8 @@ const App = () => {
 
   console.log(data);
 
-class App extends Component {
-  render() {
-    return (
-      <Router>
-        <div className='App'>
-          <h2>Prueba de static web app para reseteo de password</h2>
-          <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <ul className="navbar-nav mr-auto">
-              <li><Link to={'/home'} className="nav-link"> Comprobar usuario </Link></li>
-              <li><Link to={'/contact'} className="nav-link">Contact</Link></li>
-              <li><Link to={'/about'} className="nav-link">About</Link></li>
-            </ul>
-          </nav>
-          <hr />
-          <Routes>
-            <Route exact path='/' element={<Home />} />
-            <Route path='/contact' element={<Contact />} />
-            <Route path='/about' element={<About />} />
-          </Routes>
-        </div>
-        <div>
+  return (
+    <div>
       {err && <h2>{err}</h2>}
 
       <button onClick={handleClick}>Fetch data</button>
@@ -78,10 +53,7 @@ class App extends Component {
         );
       })}
     </div>
-      </Router>
-    );
-  }
-}
+  );
+};
 
 export default App;
-
