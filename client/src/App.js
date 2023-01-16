ddf22bffd352666dd6ed5218be322d744d5b96b8
dcarneros <theDn2000@gmail.com>
2023-01-16 12:43:19 +0100

    clean v1

--- client/src/App.js
+++ client/src/App.js
@@ -201,11 +201,6 @@ function App() {
     <div class="login__field">
       <i class="login__icon fas fa-user"></i>
       <input type={primerForm} name="email" class="login__input" onChange={handleChange} value={message} placeholder={pinmessage} />
-
-      <select id="displayedform" class="login__selector" name="typepins" >
-        <option value="pJefe">send PIN to manager's email</option>
-        <option value="pSMS">send PIN by SMS</option>
-      </select>
     
       <select id="segundoForm" class="login__selector" name="typepins" >
         <option value="pJefe">send PIN to manager's email</option>
