df18ba840b37e1b3bd9639b856470c2d911a8492
dcarneros <theDn2000@gmail.com>
2023-01-25 17:35:26 +0100

    NEW_VERSION V5

--- client/src/App.js
+++ client/src/App.js
@@ -103,7 +103,7 @@ function App() {
     // Depending if the user is verified or not, the request when pressing the button will be different
     if (pinsent === true) {
       // Tercera vez que pulsas el botón
-      };
+      
       // Enviamos petición para que se compare el PIN introducido con el que se ha enviado (se envía sólo el mail a la petición)
       axios.post('https://testpasswordapi.azure-api.net/testpasswordfunctions/getpin', data3, config) // Esta función nos debe decir si el pin es correcto o no AQUÍ HAY QUE PASARLE PIN Y NO DATA
       .then((res) => {
